2bc2167215b06f24408f5c8d210cc282c6bc6330
jessicahartman <jessica.h.hartman@duke.edu>
2019-05-16 14:42:58 -0400

    Deleting the conflicted change

--- src/plotData.R
+++ src/plotData.R
@@ -7,5 +7,4 @@ ggplot(gmpts, aes(longitude, latitude, group = status))+
   facet_grid(~ status, labeller = label_value) +
   stat_density_2d(aes(fill = ..level..), geom = "polygon", color = "black", size = 0.2, alpha = 0.5) +
   geom_point(color = "navy", size = .5, alpha = .4)
-#making a change
-#making a different change
+
